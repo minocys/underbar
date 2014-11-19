68b33a3cc89240ae57bb8d20fbe69f4de07d50f9
minocys <harich.leung@gmail.com>
2014-11-19 13:54:39 -0500

    completed extend.

--- src/underbar.js
+++ src/underbar.js
@@ -249,6 +249,17 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    console.log('hello');
+    for(var i = 1; i <= arguments.length; i++){
+      var x = 0;
+      for(var item in arguments[i]){
+        obj[item] = arguments[i][item];
+        console.log(arguments[i][item]);
+        console.log(obj);
+        x++;
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
